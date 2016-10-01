69ff61a152963ce4a4629e045ed8736249d11e94
James Hoak <jah272@case.edu>
2016-10-01 01:20:23 -0400

    Begin refactoring + re-documenting everything

--- djentils.py
+++ djentils.py
@@ -1,97 +1,135 @@
+"""
+This module holds functions that are used by the djenterator module by default
+to djenterate new songs. The functions rely on various probabilities defined
+in this file to generate random tabs; these probabilities are public and can
+be adjusted to your liking. The number of notes in a phrase can be adjusted,
+too.
+"""
+
 import random
 import music
 
-# Default rates for random effect rolls (may be changed):
-note_rate = 0.5
-zero_rate = 0.75
-mute_rate = 0.5
-hammerpull_rate = 0.2
-measure_repeat_rate = 0.55
-tremolo_rate = 0.4
-phrase_repeat_rate = 0.5
+# Options (can be adjusted)
+notes_per_phrase = 64       # Or 4x the number of notes per measure.
+
+# Probability Name          # Frequency of...
+note_rate = 0.5             # ...normal notes
+zero_rate = 0.75            # ...open-string notes (as opposed to 1st-frets)
+mute_rate = 0.5             # ...palm-muted notes
+hammerpull_rate = 0.2       # ...hammer-ons/pull-offs
+measure_repeat_rate = 0.55  # ...repeating first 1/2 of phrase in second 1/2
+tremolo_rate = 0.4          # ...tremolo-picking
+phrase_repeat_rate = 0.5    # ...repeating an entire phrase in next phrase
 
 # Other globals
-default_tuning = music.Tuning("drop", "a")
-notes_per_phrase = 64
-_bad_endings = ['h', 'p']
+_default_tuning = music.Tuning("drop", "a")
 _roll = random.random
+_bad_endings = ['h', 'p']   # Phrases cannot end in these
 
-def _default_note_ls():
-	return ['-'] * notes_per_phrase
+def _empty_note_ls():
+    """Returns an "empty line" (see music.py)."""
+    return ['-'] * notes_per_phrase
 
 def _roll_note_and_mute():
-	note = '0' if _roll() < zero_rate else '1'
-	mute = 'm' if _roll() < mute_rate else '-'
-	return note, mute
+    """Picks the next note (0 or 1) and may/may not mute it."""
+    note = '0' if _roll() < zero_rate else '1'
+    mute = 'm' if _roll() < mute_rate else '-'
+    return note, mute
 
 def _roll_tremolo(notes, mutes, index, end):
-	while index < end - 1:
-		if _roll() < note_rate:
-			notes[index], mutes[index] = _roll_note_and_mute()
-			index += 1
-		else:
-			break
-	return index
+    """Adds notes to a tremolo section; might add none by chance."""
+    while index < end - 1:
+        if _roll() < note_rate:
+            notes[index], mutes[index] = _roll_note_and_mute()
+            index += 1
+        else:
+            break
+    return index
 
 def _add_hammerpull(notes, mutes, index):
-	notes[index:index + 2] = ['h', '1'] if notes[index - 1] == '0' else ['p', '0']
-	if _roll() < mute_rate:
-		mutes[index + 1] = 'm'
-	return index + 2
+    """
+    Adds a hammer-on/pull off depending on last note.
+
+    The resulting note might be muted. Also the first note might be muted.
+    So you can hammer-on a muted open string into a nonmuted 1st-fret. 
+    ...Why? Well, why not?
+    """
+
+    toadd = ['h', '1'] if notes[index - 1] == '0' else ['p', '0']
+    notes[index:index + 2] = toadd
+    if _roll() < mute_rate:
+        mutes[index + 1] = 'm'
+    return index + 2
 
 def _roll_effects(notes, mutes, start_index):
-	adding = True
-	index, end = start_index, len(notes)
-	while adding and index < end - 1:
-		tremolo = _roll() < tremolo_rate
-		hammerpull = _roll() < hammerpull_rate
-		if tremolo:
-			index = _roll_tremolo(notes, mutes, index, end)
-		if hammerpull and index < end - 1:
-			index = _add_hammerpull(notes, mutes, index)
-		if not tremolo and not hammerpull:
-			adding = False
-	return index
+    """
+    Uses probability to add tremolo or hammer-on/pulloff after the last note.
+    """
+    adding = True
+    index, end = start_index, len(notes)
+    while adding and index < end - 1:
+        # According to chance, maybe add tremolo and maybe add HO/POs.
+        tremolo = _roll() < tremolo_rate
+        hammerpull = _roll() < hammerpull_rate
+        if tremolo:
+            index = _roll_tremolo(notes, mutes, index, end)
+        if hammerpull and index < end - 1:
+            index = _add_hammerpull(notes, mutes, index)
+        
+        # If nothing was added this iteration, stop adding effects
+        if not tremolo and not hammerpull:
+            adding = False
+    return index
 
 def _roll_repeated_measures(notes, mutes):
-	if _roll() < measure_repeat_rate:
-		halfway = len(notes) // 2
-		notes[halfway:] = notes[:halfway]
-		mutes[halfway:] = mutes[:halfway]
-		if notes[halfway-1] in _bad_endings:
-			notes[halfway-1] = '-'
-			notes[-1] = '-'
+    """Repeat the 1st 1/2 of a phrase in the 2nd 1/2 (according to chance."""
+    if _roll() < measure_repeat_rate:
+        halfway = len(notes) // 2
+        notes[halfway:] = notes[:halfway]
+        mutes[halfway:] = mutes[:halfway]
 
-def gen_phrase():
-	lists = (_default_note_ls() for i in range(2))
-	notes, mutes = lists
-	index = 0
-	while index < notes_per_phrase:
-		if _roll() < note_rate:
-			# Maybe add note and maybe a mute
-			notes[index], mutes[index] = _roll_note_and_mute()
-			index += 1
+        # If we end on an invalid hammer-on/pull off 'h' or 'p', reroll a note
+        if notes[halfway-1] in _bad_endings:
+            notes[halfway-1], mutes[halfway-1] = _roll_note_and_mute()
 
-			# Maybe add other effects, too
-			index = _roll_effects(notes, mutes, index)
-		else:
-			index += 1
-	#_roll_repeated_measures(notes, mutes)
-	return notes, mutes
+def _gen_phrase():
+    """Generates a phrase of a song."""
+    notes = _empty_note_ls()
+    mutes = _empty_note_ls()
+    index = 0
+    while index < notes_per_phrase:
+        # Add a note (by probability). If successful, add effects
+        if _roll() < note_rate:
+            notes[index], mutes[index] = _roll_note_and_mute()
+            index += 1
 
-def gen_song(min_phrases, max_phrases=None, tuning=default_tuning):
-	num_phrases = 0
-	song = music.Song(tuning)
-	if not max_phrases:
-		max_phrases = float("inf")
-	while num_phrases < min(min_phrases, max_phrases):
-		# Generate a phrase and add to the song
-		presets = gen_phrase()
-		song.add_phrase(presets)
-		num_phrases += 1
+            index = _roll_effects(notes, mutes, index)
+        # If unsuccessful, just move to the next note position in the tab
+        else:
+            index += 1
+    return notes, mutes
 
-		# Duplicate it if we need to
-		while random.random() < phrase_repeat_rate and num_phrases < max_phrases:
-			song.add_phrase(presets)
-			num_phrases += 1
-	return song
+def gen_song(min_phrases, max_phrases=None, tuning=_default_tuning):
+    """
+    Generates a new song (series of phrases) and returns it.
+
+    Song length here is just measured by the number of phrases (no time or
+    tempo involved). Probabilities used by this generator are, as stated,
+    described above.
+    """
+    num_phrases = 0
+    song = music.Song(tuning)
+    if not max_phrases:
+        max_phrases = float("inf")
+
+    # Generally, djenterate at least min_phrases but no more than max
+    while num_phrases < min(min_phrases, max_phrases):
+        notes, mutes = gen_phrase()
+        song.add_phrase(notes, mutes)
+        num_phrases += 1
+
+        # By chance, we can also repeat a phrase more than once
+        while random.random() < phrase_repeat_rate and num_phrases < max_phrases:
+            song.add_phrase(presets)
+            num_phrases += 1
+    return song
--- music.py
+++ music.py
@@ -1,183 +1,231 @@
 """
-This module provides abstractions for musical structures, i.e.
-the chromatic scale, phrases, guitar tunings, and entire songs (many phrases).
+This module contains abstractions of common musical constructs, to be used
+in djenteration.
 """
 
-# Globals: Chromatic scale notes and possible tuning types
-chromatic_notes = ('a', 'a#', 'b', 'c', 'c#', 'd', 
-				   'd#', 'e', 'f', 'f#', 'g', 'g#')
-tuning_types = ('drop', 'standard')
+# Notes on the chromatic scale
+chromatic_notes = ('a', 'a#', 'b', 'c', 'c#', 'd',
+                   'd#', 'e', 'f', 'f#', 'g', 'g#')
 
 def next_note(base_note, offset):
-	"""
-	Given a note on the chromatic scale, returns the next note with given
-	offset from it.
-	"""
-	this_index = chromatic_notes.index(base_note)
-	next_index = (this_index + offset) % len(chromatic_notes)
-	return chromatic_notes[next_index]
+    """
+    Given a chromatic scale note, returns the one <offset> semitones up/down.
+
+    The base note can be anything in chromatic_notes, of course. offset should
+    be an integer.
+    """
+    base_index = chromatic_notes.index(base_note.lower())
+    next_index = (base_index + offset) % len(chromatic_notes)
+    return chromatic_notes[next_index]
 
 class Tuning:
-	"""
-	Represents a guitar tuning for any guitar with 4+ strings. Given a base
-	note and tuning type (drop or standard) as well as a number of strings,
-	this will generate the notes of the corresponding tuning. (i.e. E standard
-	becomes EADGBE for a 6-string)
-	"""
+    """Abstraction for a guitar tuning for some given # of strings."""
 
-	def __init__(self, type='standard', base_note='c#', num_strings=9):
-		"""
-		Generates a tuning given a type, base note, and number of strings on a
-		guitar.
+    def __init__(self, tuning_type='standard', base_note='c#', num_strings=9):
+        """
+        Makes a new tuning for a guitar with some # of strings.
 
-		The possible types are "drop" or "standard". Base notes can be anything
-		on the chromatic scale. The number of strings must be at least 4.
-		"""
+        Unless arguments are specified, each new tuning is aimed at a 9-string
+        guitar and is in C# standard (so the notes are EBGDAEBF#C#), a not-so- 
+        uncommon djent tuning. (This assumes all 3 extra strings are thicker
+        than the 6th string already.)
+        """
 
-		# Tuning is represented as a series of notes starting at a base note
-		self.base_note = base_note
-		self.increments = ([5] * (num_strings - 4)) + [4,5]
+        self._base_note = base_note.lower()
+        self._tuning_type = tuning_type.lower()
 
-		# Add first increment based on tuning type (drop -> 2 semitones lower)
-		self.increments.insert(0, (7 if type == 'drop' else 5))
+        # Add note for next-widest string (e.g. F# for above example tuning).
+        # This varies based on the tuning type.
+        self._notes = [base_note]
+        next_offset = 7 if tuning_type == 'drop' else 5
+        self._notes.append(next_note(base_note, next_offset))
 
-	def notes(self):
-		"""
-		Generates and returns the notes of the tuning from the base note and
-		increments.
-		"""
-		note_ls = [self.base_note]
-		for i in range(len(self.increments)):
-			note_ls.insert(len(note_ls), next_note(note_ls[i], self.increments[i]))
-		return tuple(note_ls)
+        # Now determine notes of all other strings. Typically each string is
+        # 5 semitones up from the last one (from thickest to thinnest) but
+        # the second-to-last string is only 4 semis up.
+        second_to_last_str = num_strings - 2
+        for i in range(2, num_strings):
+            next_offset = 5 if i != second_to_last_str else 4
+            base = self._notes[i - 1]
+            self._notes.append(next_note(base, next_offset))
 
+    def base_note(self):
+        """Return the base note of this tuning (thickest string's note)."""
+        return self._base_note
 
-class PhraseLine:
-	"""
-	Represents the notes on a particular string that are to be played in one
-	phrase. Because this is the djenterator, all but 1 string will be silent
-	during every single song.
-	"""
-	def __init__(self, base_note, num_notes):
-		"""
-		Initialized using the string's base note and the number of notes
-		that will be played in this phrase (4 measures).
-		"""
+    def tuning_type(self):
+        """Return this tuning's type."""
+        return self._tuning_type
 
-		self.base_note = base_note
-		self.notes = ['-'] * num_notes
+    def _notes(self):
+        """Return the notes of this tuning."""
+        return tuple(self._notes)
 
-	def __str__(self):
-		"""
-		Returns the string representation of this line of notes.
+def _get_qtr(i, ls):
+    """Gets a given quarter of a list. 0=first quarter, 1=second, etc."""
+    qtr = len(ls) // 4
+    start, end = i * qtr, (i + 1) * qtr
+    return ls[start:end]
 
-		Generally, it appears as a note followed by a set of four measures
-		(sets of 0s, 1s, and -'s, generally) separated by vertical lines, like:
-		A#|--01|010-|0111|10-1|
+def _get_mid(line):
+    """
+    For a Phrase/MuteLine, return the representation of the notes.
 
-		Of course, this represents the notes on just one string.
-		"""
-		start, end = self.base_note.rjust(2) + '|', '|'
-		qtr = len(self.notes) // 4
+    Typically this is like:
+    010-|-10-|1110|0001
 
-		# Generate each of the four measures, then compose each one by their
-		# notes individually then concatenate with vertical bars inbetween
-		groups = tuple(self.notes[i * qtr:(i+1) * qtr] for i in range(4))
-		groups = ["".join(groups[i]) for i in range(4)]
-		mid = "|".join(groups)
-		return start + mid + end
+    This is a convenience function to help print phrase/mute lines.
+    """
+    groups = tuple(_get_qtr(i, line.notes) for i in range(4))
+    groups = ["".join(groups[i]) for i in range(4)]
+    return "|".join(groups)
 
+class _PhraseLine:
+    """
+    Line of notes on one string, used to build phrases.
 
-class MuteLine:
-	"""Same as a phrase line, but this indicates which notes will be muted."""
+    Notes are ordered by the time they are played, so the sequence 0111 means
+    that a 0 is played, then three 1's are played. (In case you're wondering,
+    a 0 is when you play an open string (don't hold any guitar frets) and a 1
+    is when you hold the first fret of a string and strum it.)
 
-	def __init__(self, num_notes):
-		"""Creates a mute line. Only checks that the number of notes is OK."""
-		self.notes = ['-'] * num_notes
+    According to djent tradition, only the fattest string on the guitar will
+    be played, and the guitarist may only play the open string or with the
+    first fret held down.
+    """
 
-	def __str__(self):
-		"""Makes a String representation of the line.
+    def __init__(self, base_note, num_notes):
+        """
+        Form a PhraseLine given a string's note and the max # of notes.
 
-		The resulting representation will look like the 2nd line in the
-		following example:
+        The '-' symbol conventionally means nothing is to be played at that
+        time.
+        """
+        self.base_note = base_note.lower()
+        self.notes = ['-'] * num_notes
 
-		C#|--01|010-|0111|10-1|
-		  {--mm|m-m-|--mm|mm--}
+    def __str__(self):
+        """
+        Return the string representation of this line.
 
-		Here, 'm' says a note should be muted, but the '-' indicates that
-		a note should not be.
-		"""
-
-		start, end = '  {', '}'
-		qtr = len(self.notes) // 4
-		
-		# Generate each of the four measures, then compose each one by their
-		# notes individually then concatenate with vertical bars inbetween
-		groups = tuple(self.notes[i * qtr:(i+1) * qtr] for i in range(4))
-		groups = ["".join(groups[i]) for i in range(4)]
-		mid = "|".join(groups)
-		return start + mid + end
+        These typically look something like:
+        C#|0101|100-|0101|011-|
+        which means the string is C#, and there are 14 notes played over
+        four measures (which together define one phrase).
+        """
+        start, end = self.base_note.rjust(2) + '|', '|'
+        mid = _get_mid(self)
+        return start + mid + end
 
 
-class Phrase:
-	"""
-	Represents a musical phrase; in this simple case, this is assumed to be
-	just 4 measures. 
+class _MuteLine:
+    """
+    Like PhraseLines, but specifies notes that will be played but muted.
 
-	(Note: Measures are not explicitly defined; when we generate
-	notes, we generate them for the whole phrase, and they are separated
-	into measures afterwards.)
-	"""
+    On a new tab this line will look like:
+    C#|0101|100-|0101|011-|
+      {--mm|m---|----|-m--}
 
-	def __init__(self, tuning, num_notes, num_strings):
-		"""
-		Creates and initializes a new phrase given a specific tuning and
-		number of notes and strings to use.
-		"""
+    The 2nd line in the example represents the actual MuteLine and says that
+    the above note will be muted. (The 1st line of course is just a Phrase
+    Line.)
+    """
+    def __init__(self, num_notes):
+        """
+        Given the max # of notes for this phrase, make a MuteLine.
 
-		notes = tuning.notes()
-		strs = num_strings
-		self.num_strings = strs
-		self.lines = [PhraseLine(notes[strs-i-1], num_notes) for i in range(strs)]
-		self.mute_line = MuteLine(num_notes)
+        Like PhraseLines, '-' means don't mute the note above it.
+        """
+        self.notes = ['-'] * num_notes
 
-	def set_notes(self, preset_notes):
-		"""
-		Sets the notes of this phrase according to a given preset, including
-		both notes and mutes.
+    def __str__(self):
+        """Returns the string representation of this line, described above."""
+        start, end = '  {', '}'
+        mid = _get_mid(self)
+        return start + mid + end
 
-		The presets must contain exactly two elements. The first is the line of
-		notes corresponding to the notes to be played on the last string. The
-		second is the sequence of mutes associated with the given notes. Of
-		course, these must be of the same length.
-		"""
 
-		last_string = self.lines[self.num_strings-1]
-		last_string.notes = preset_notes[0]
-		self.mute_line.notes = preset_notes[1]
+class _Phrase:
+    """A musical phrase, represented here by a set of four measures."""
 
-	def __str__(self):
-		"""Returns this as a string, e.g. phrase + mute lines joined by newlines."""
-		lines = [str(line) for line in (self.lines + [self.mute_line])]
-		return '\n'.join(lines)
+    def __init__(self, tuning, num_notes, num_strings):
+        """
+        Make a new phrase, given a tuning, max # of notes, and # of strings.
+        """
+        
+        tuning_notes = tuning._notes()
+        strs = num_strings
+        self.num_strings = strs
+
+        def make_phrase_line(string_num):
+            """
+            Makes a PhraseLine given a base note and # of notes in a phrase.
+            """
+            base_note = tuning_notes[strs - string_num - 1]
+            return _PhraseLine(base_note, num_notes)
+
+        # Initialize each line in the phrase (1 for each guitar string)
+        self.lines = [make_phrase_line(i) for i in range(strs)]
+        self.mute_line = _MuteLine(num_notes)
+
+    def _set_notes(self, notes, mutes):
+        """Set the notes + mutes for this phrase, given them as arguments."""
+        last_string = self.lines[self.num_strings-1]
+        last_string.notes = notes
+        self.mute_line.notes = mutes
+
+    def __str__(self):
+        """
+        Returns the string representation of this phrase.
+
+        Generally, phrases look like:
+         E|----|----|----|----|
+         B|----|----|----|----|
+         G|----|----|----|----|
+         D|----|----|----|----|
+         A|----|----|----|----|
+         E|0101|100-|0101|011-|
+          {--mm|m---|----|-m--}
+
+        Again, due to ancient djent tradition, no strings other than the
+        widest one are to be played, which explains why notes are only on
+        the lower E string here.
+        """
+        lines = [str(line) for line in (self.lines + [self.mute_line])]
+        return '\n'.join(lines)
 
 class Song:
-	"""
-	Represents a collection of phrases, which are in turn a collection of lines
-	on each string (over multiple measures), which are a collection of a
-	specified number of notes.
-	"""
+    """
+    Represents a series of phrases, in the simplest sense.
 
-	def __init__(self, tuning):
-		"""Creates a new Song, though phrases must be added manually."""
-		self.phrase_ls = []
-		self.tuning = tuning
+    Each song initially has no phrases, but add_phrase() calls are used
+    to build it up.
+    """
 
-	def add_phrase(self, presets):
-		p = Phrase(self.tuning, len(presets[0]), len(self.tuning.notes()))
-		p.set_notes(presets)
-		end_index = len(self.phrase_ls)
-		self.phrase_ls.insert(end_index, p)
+    def __init__(self, tuning):
+        """Makes an empty song given a guitar tuning to use."""
 
-	def __str__(self):
-		return '\n\n'.join([str(meas) for meas in self.phrase_ls])
+        self._phrase_ls = []
+        self._tuning = tuning
+
+    def tuning():
+        """Returns this song's tuning."""
+        return self._tuning
+
+    def add_phrase(self, notes, mutes):
+        """Adds a phrase to this, given a set of notes and mutes to use."""
+        num_notes = len(presets[0])
+        num_strings = len(self._tuning._notes())
+
+        p = _Phrase(self._tuning, num_notes, num_strings)
+        p._set_notes(notes, mutes)
+        self._phrase_ls.append(p)
+
+    def __str__(self):
+        """
+        Returns the string representation of this song.
+
+        Each song is, as stated, a series of phrases. It is simply represented
+        as all of the phrases with two newlines between each pair.
+        """
+        return '\n\n'.join([str(p) for p in self_.phrase_ls])
